c076aafe4eb507a9d3a3fd59abefd8a89c7bafdd
harsh-vora9 <83445984+harsh-vora9@users.noreply.github.com>
2023-01-04 15:21:37 +0530

    BE endpoint

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('http://localhost:5001', {
+  const response = await fetch('https://hv-open-ai-chatgtp.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
