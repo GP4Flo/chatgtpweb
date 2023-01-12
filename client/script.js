693995ab534f0fa8ce2e22a2a5258efb4e8af598
GP4Flo <73472206+GP4Flo@users.noreply.github.com>
2023-01-12 12:33:51 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('https://chatgtpweb.vercel.app', {
+  const response = await fetch('https://hv-open-ai-chatgtp.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
